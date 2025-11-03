fa069aa2dd95f47a63d5bb2ef5c158e4e4457969
elouairimarouan <elouairi.marouan@gmail.com>
2025-11-03 03:29:24 +0100

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ const getAllUsers = async (req, res) => {
     const users = await User.find().select('-password');
 
     res.status(200).json({
-      success: true,
+      success: 'nothig',
       data: users
     });
   } catch (error) {
